b81f9fe671f49220dcc4e1a5e09f24b05a72d67d
이현서 <leehyunseo@Leehyunseo-Mac.local>
2020-06-25 14:10:36 +0900

    Updates

---
+++ precache-manifest.742f9e79f9a85526dbeb0a540eb98c6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21252b4d8df5ed0de03090a30a4a7229",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dea09c49f5f8f69e8794",
+    "url": "/static/css/main.d1eca7df.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dea09c49f5f8f69e8794",
+    "url": "/static/js/main.98433f07.chunk.js"
+  },
+  {
+    "revision": "bd7f4f55b55e38cea2f1",
+    "url": "/static/js/runtime-main.58002af7.js"
+  }
+]);
